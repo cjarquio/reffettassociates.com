78c09d96a7b33883ebd5cf86b5352257e77c17ca
Charles Jarquio <cjarquio@gmail.com>
2020-06-10 21:12:23 -0700

    Attempt at mapping offices

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+import locations from '../assets/data/locations';
 
-export default function HomePage(){
-  return(
+export default function HomePage() {
+  return (
     <div>
-      This is the landing page.
+      {
+        locations.map((office) => {
+          return (
+            <Card key={office.id}>
+              <CardImg top width="100%" src="reffettassociates/src/assets/images/Seattle.jpg" alt={office.location} />
+              <CardText>{office.address}</CardText>
+            </Card>
+          )
+        })
+      }
     </div>
   );
 }
