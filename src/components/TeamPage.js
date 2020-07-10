38726530c26b0dc2d01b0dd32e15d87bf3861133
CJ Jarquio <cjarquio@gmail.com>
2020-07-09 17:16:54 -0700

    Added tabs for team

--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { AppBar, Tabs, Tab, Box, Typography } from '@material-ui/core'
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import PageTitle from './PageTitle';
 import team from '../assets/data/team.json';
 
-const useStyles = makeStyles(() =>
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    'aria-controls': `scrollable-force-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) =>
   createStyles({
+    root: {
+      flexGrow: 1,
+      width: '100%',
+      backgroundColor: theme.palette.background.paper,
+    },
     picture: {
       width: '100%'
     },
@@ -18,24 +58,31 @@ const subtitle = `Our team of experienced and trusted executive search experts a
 
 export default function TeamPage() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const RenderTeamMembers = () => {
     return (
       team.map((member) => {
         return (
-          <div style={{ borderBottom: '2px solid black', paddingTop: 10 }} className='row' key={member.id}>
-            <div className='col-12 col-md-2'>
-              <img className={classes.picture} src={member.image} alt={member.name} />
-              <p>
-                {member.name}<br />
-                {member.title}<br />
-                {member.location}
-              </p>
+          <TabPanel value={value} index={member.id}>
+            <div style={{ paddingTop: 10 }} className='row' key={member.id}>
+              <div className='col-12 col-md-2'>
+                <img className={classes.picture} src={member.image} alt={member.name} />
+                <p>
+                  {member.name}<br />
+                  {member.title}<br />
+                  {member.location}
+                </p>
+              </div>
+              <div className='col-12 col-md-10'>
+                <RenderBio bio={member.bio} />
+              </div>
             </div>
-            <div className='col-12 col-md-10'>
-              <RenderBio bio={member.bio} />
-            </div>
-          </div>
+          </TabPanel>
         );
       })
     );
@@ -55,6 +102,27 @@ export default function TeamPage() {
         title='Our Team | Experienced, Professional, &amp; Invested In Your Goals'
         subtitle={subtitle}
       />
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="on"
+            indicatorColor="primary"
+            textColor="primary"
+            aria-label="scrollable force tabs example"
+          >
+            {
+              team.map((member) => {
+                return (
+                  <Tab label={member.name} {...a11yProps(member.id)} />
+                );
+              })
+            }
+          </Tabs>
+        </AppBar>
+      </div>
       <RenderTeamMembers />
     </div>
   );
