222e429834bebab27ed7299ebb2e8676d0b2fc34
CJ Jarquio <cjarquio@gmail.com>
2020-07-10 10:43:40 -0700

    Added Contact form

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import PageTitle from './PageTitle';
+import ContactForm from './ContactForm';
 
-export default function ContactPage(){
-  return(
+export default function ContactPage() {
+  return (
     <div>
-      This is the Contact page.
-      Add seattle contact here
+      <PageTitle
+        title='Cotact Us'
+        subtitle=''
+      />
+      Main Office (Seattle)
+      <br/><br/>
+      11900 NE 1st Street, Suite 300<br/>
+      Bellevue, WA 98005<br/>
+      Phone: (425) 637-2993<br/>
+      Fax: (425) 869-5347
+      <br/><br/>
+      <ContactForm />
     </div>
   );
 }
