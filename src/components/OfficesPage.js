9129b25c90f3986b97d4263d15eeb1a54289c028
CJ Jarquio <cjarquio@gmail.com>
2020-07-17 09:38:09 -0700

    Centered Greensboro office, Changed Dallas locaiton name

--- src/components/OfficesPage.js
+++ src/components/OfficesPage.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Card, CardImg, CardHeader, NavLink } from 'reactstrap';
+import { Card, CardImg, CardHeader, CardText, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import locations from '../assets/data/locations';
 
 const useStyles = makeStyles(() =>
   createStyles({
+    root: {
+      flex:'0 0 50%',
+      maxWidth:'50%',
+      '&:last-child:nth-child(odd)': {
+        transform:'translateX(50%)'
+      }
+    },
     officeCards: {
       height: 300,
-      margin: 25
+      margin: 25,
+      paddingBottom: '30px'
     },
     cardImage: {
       height: '100%',
@@ -20,6 +28,11 @@ const useStyles = makeStyles(() =>
       alignSelf: 'center',
       fontSize: '1.1em',
       padding: '0.4rem 1.25rem'
+    },
+    cardText: {
+      color: 'black',
+      alignSelf: 'center',
+      fontSize: '1em',
     }
   })
 );
@@ -33,11 +46,12 @@ export default function OfficesPage() {
         {
           locations.map((office) => {
             return (
-              <div className="col-12 col-sm-6">
+              <div className={classes.root}>
                 <NavLink style={{ height: 'inherit' }} tag={Link} to="/contact">
                   <Card className={classes.officeCards} key={office.id}>
                     <CardHeader className={classes.cardTitle}>{office.location}</CardHeader>
                     <CardImg top className={classes.cardImage} src={office.image} alt={office.location} />
+                    <CardText className={classes.cardText}>{office.address}</CardText>
                   </Card>
                 </NavLink>
               </div>
