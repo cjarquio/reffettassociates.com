d0250796d998982a4ea4d1b04c890e94486cce0e
CJ Jarquio <cjarquio@gmail.com>
2020-07-17 10:38:15 -0700

    Fixed issue with offices

--- src/components/OfficesPage.js
+++ src/components/OfficesPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {isBrowser} from 'react-device-detect';
 import { Card, CardImg, CardHeader, CardText, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
@@ -7,6 +8,7 @@ import locations from '../assets/data/locations';
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
+      display: isBrowser? 'flex':'contents',
       flex:'0 0 50%',
       maxWidth:'50%',
       '&:last-child:nth-child(odd)': {
