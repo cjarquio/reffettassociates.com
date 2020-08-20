bbef87b2b7cab61a33cc9812eeb539d224ef423c
CJ Jarquio <cjarquio@gmail.com>
2020-08-20 10:55:45 -0700

    Changed main navlink colors

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -37,31 +37,31 @@ export default function MainNavigation() {
         <Collapse isOpen={isOpen} navbar>
           <Nav className={classes.navOptions} navbar>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/about'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/about">About</NavLink>
+              <NavLink style={{color: location.pathname === '/about'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/about">About</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/team'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/team">Our Team</NavLink>
+              <NavLink style={{color: location.pathname === '/team'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/team">Our Team</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/services'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/services">Services</NavLink>
+              <NavLink style={{color: location.pathname === '/services'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/services">Services</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/veterans'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/veterans">Veterans</NavLink>
+              <NavLink style={{color: location.pathname === '/veterans'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/veterans">Veterans</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/process'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/process">Process</NavLink>
+              <NavLink style={{color: location.pathname === '/process'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/process">Process</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/offices'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/offices">Offices</NavLink>
+              <NavLink style={{color: location.pathname === '/offices'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/offices">Offices</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/eta'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/eta">Executive Transition Assistance</NavLink>
+              <NavLink style={{color: location.pathname === '/eta'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/eta">Executive Transition Assistance</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/contact'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/contact">Contact</NavLink>
+              <NavLink style={{color: location.pathname === '/contact'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/contact">Contact</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color: location.pathname === '/news'? 'rgba(0,0,0,0.8)':'rgba(0,0,0,0.5)'}} tag={Link} to="/news">News</NavLink>
+              <NavLink style={{color: location.pathname === '/news'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/news">News</NavLink>
             </NavItem>
           </Nav>
         </Collapse>
