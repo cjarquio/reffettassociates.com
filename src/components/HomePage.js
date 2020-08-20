d90b7f47ce319b650ba375010e0760e0154e9cd8
CJ Jarquio <cjarquio@gmail.com>
2020-08-20 09:16:18 -0700

    Locations on home page lead to contacts

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Carousel, CarouselItem, CarouselControl } from 'reactstrap';
+import { useHistory } from "react-router-dom";
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() =>
@@ -14,28 +15,12 @@ const useStyles = makeStyles(() =>
     banner: {
       width: '100%'
     },
-    bannerImage: {
-      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url('/images/Banner/Landscape.jpg')`,
-      height: '50%',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      position: 'relative',
-      paddingTop: 30,
-      paddingBottom: 30
-    },
-    bannerText: {
-      margin: 'auto',
-      color: 'white',
-      width: '60%',
-      fontFamily: 'Georgia',
-      fontStyle: 'italic'
-    },
     locations: {
       textAlign: 'center',
       color: '#0f2f5b',
       fontFamily: 'Georgia',
-      fontWeight: 'bold'
+      fontWeight: 'bold',
+      cursor: 'pointer'
     }
   })
 );
@@ -70,6 +55,7 @@ const items = [
 
 export default function HomePage() {
   const classes = useStyles();
+  const history = useHistory();
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
@@ -97,13 +83,12 @@ export default function HomePage() {
     );
   });
 
+  const handleClick = () => {
+    history.push("/contact");
+  }
+
   return (
     <div>
-      <div className={classes.bannerImage}>
-        <div className={classes.bannerText}>
-          <h1>A LEADER IN EXECUTIVE SEARCH</h1>
-        </div>
-      </div>
       <Carousel
         activeIndex={activeIndex}
         next={next}
@@ -114,7 +99,7 @@ export default function HomePage() {
         <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
         <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
       </Carousel>
-      <h4 className={classes.locations}>Seattle | Washington D.C. | New York | Dallas | Greensboro</h4>
+      <h4 className={classes.locations} onClick={handleClick}>Seattle | Washington D.C. | New York | Dallas | Greensboro</h4>
     </div>
   );
 }
