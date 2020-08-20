b506df5319ef889cbca8171636ccea6a6c31c9a8
CJ Jarquio <cjarquio@gmail.com>
2020-08-20 09:46:46 -0700

    Added new home image

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { Carousel, CarouselItem, CarouselControl } from 'reactstrap';
+import React from 'react';
 import { useHistory } from "react-router-dom";
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 
@@ -20,68 +19,16 @@ const useStyles = makeStyles(() =>
       color: '#0f2f5b',
       fontFamily: 'Georgia',
       fontWeight: 'bold',
-      cursor: 'pointer'
+      cursor: 'pointer',
     }
   })
 );
 
-const items = [
-  {
-    src: '/images/HomeSlides/ReffettAssociatesHome.jpg',
-    altText: 'Slide 1',
-    caption: 'Slide 1'
-  },
-  {
-    src: '/images/HomeSlides/Crowd.JPG',
-    altText: 'Slide 2',
-    caption: 'Slide 2'
-  },
-  {
-    src: '/images/HomeSlides/Diverse.JPG',
-    altText: 'Slide 3',
-    caption: 'Slide 3'
-  },
-  {
-    src: '/images/HomeSlides/Efficient.JPG',
-    altText: 'Slide 4',
-    caption: 'Slide 4'
-  },
-  {
-    src: '/images/HomeSlides/Expansive.jpg',
-    altText: 'Slide 5',
-    caption: 'Slide 5'
-  }
-];
+
 
 export default function HomePage() {
   const classes = useStyles();
   const history = useHistory();
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  }
-
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  }
-
-  const slides = items.map((item) => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-        <img className="d-block w-100" src={item.src} alt={item.altText} />
-      </CarouselItem>
-    );
-  });
 
   const handleClick = () => {
     history.push("/contact");
@@ -89,16 +36,7 @@ export default function HomePage() {
 
   return (
     <div>
-      <Carousel
-        activeIndex={activeIndex}
-        next={next}
-        previous={previous}
-        interval={3000}
-      >
-        {slides}
-        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-      </Carousel>
+      <img src='\images\HomeSlides\ReffettAssociatesHome.jpg' alt='Reffett Associate Landing Page' className={classes.banner}/>
       <h4 className={classes.locations} onClick={handleClick}>Seattle | Washington D.C. | New York | Dallas | Greensboro</h4>
     </div>
   );
