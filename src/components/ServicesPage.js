33de79c26290a85c3036f961e011f41964031d40
CJ Jarquio <cjarquio@gmail.com>
2020-09-10 16:54:45 -0700

    Fixed typo

--- src/components/ServicesPage.js
+++ src/components/ServicesPage.js
@@ -5,7 +5,7 @@ const subtitle = `We believe that every search is a partnership with our client 
 \n\n
 Our goal as a small firm is to be able to provide a high level of personal attention and unparalleled service to both our clients and our candidates. For the Reffett Associates team, executive recruiting is all about relationships. Building and maintaining long-term, productive relationships with everyone with whom we work is our first priority. 
 \n\n
-Through our time tested process we will work with you to define the role, go to market, identify and recruit a diverse talent pool and navigate the interview and hiring process. On Average you will see your first group of candidates within 5 weeks of our Kick-off meeting. `;
+Through our time tested process we will work with you to define the role, go to market, identify and recruit a diverse talent pool and navigate the interview and hiring process. On average you will see your first group of candidates within 5 weeks of our Kick-off meeting. `;
 
 export default function ServicesPage(){
   return(
