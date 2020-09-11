784d441e9fb7bfe03cdfeae677df9f3074e307c8
CJ Jarquio <cjarquio@gmail.com>
2020-09-10 17:33:38 -0700

    close nav menu when navigating to different page on mobile

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -36,31 +36,31 @@ export default function MainNavigation() {
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className={classes.navOptions} navbar>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/about'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/about">About</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/team'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/team">Our Team</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/services'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/services">Services</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/process'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/process">Process</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/veterans'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/veterans">Veterans</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/offices'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/offices">Offices</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/eta'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/eta">Executive Transition Assistance</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/contact'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/contact">Contact</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={toggle}>
               <NavLink style={{color: location.pathname === '/news'? 'rgba(207,32,47,1)':'rgba(207,32,47,0.7)'}} tag={Link} to="/news">News</NavLink>
             </NavItem>
           </Nav>
