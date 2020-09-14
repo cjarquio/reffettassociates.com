057159e0b4388f0c1604617ec582df1cda9a8e5c
CJ Jarquio <cjarquio@gmail.com>
2020-09-14 12:45:32 -0700

    Reverted Home page back to just image

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from "react-router-dom";
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators
-} from 'reactstrap';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import practiceAreas from '../assets/data/services.json';
-import { Typography } from '@material-ui/core';
-import { isBrowser } from 'react-device-detect';
 
 const useStyles = makeStyles(() =>
   createStyles({
-    root: {
-    },
     officeCards: {
       height: 300,
       margin: 25
@@ -23,9 +12,7 @@ const useStyles = makeStyles(() =>
       height: '100%'
     },
     banner: {
-      width: '100%',
-      height: '533px',
-      maxHeight: '350px'
+      width: '100%'
     },
     locations: {
       textAlign: 'center',
@@ -33,86 +20,23 @@ const useStyles = makeStyles(() =>
       fontFamily: 'Georgia',
       fontWeight: 'bold',
       cursor: 'pointer',
-    },
-    caption: {
-      color: 'white',
-      textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
-      position: 'absolute',
-      top: '85%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      fontSize: isBrowser ? '1.55em' : '1em',
-      fontFamily: 'Georgia'
-    },
-    controls: {
-      backgroundColor: 'rgba(0,0,0,0.1)'
     }
   })
 );
 
+
+
 export default function HomePage() {
   const classes = useStyles();
   const history = useHistory();
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-  const initialSlide = {
-    "srcImg": "/images/Home/ReffettAssociatesHome.jpg",
-    "altText": "",
-    "serviceArea": "",
-    "url": "",
-    "description": "",
-    "clients": [],
-    "subtitle": "",
-    "subtitleList": []
-  };
-  const homeSlides = [initialSlide].concat(practiceAreas);
 
   const handleClick = () => {
     history.push("/contact");
   }
 
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === homeSlides.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  }
-
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? homeSlides.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  }
-
-  const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  }
-
-  const slides = homeSlides.map((item) => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.altText}
-      >
-        <Typography variant='h5' className={classes.caption}>{item.serviceArea}</Typography>
-        <img src={item.srcImg} alt={item.altText} className={classes.banner} />
-      </CarouselItem>
-    );
-  });
-
   return (
-    <div className={classes.root}>
-      <Carousel
-        activeIndex={activeIndex}
-        next={next}
-        previous={previous}
-      >
-        <CarouselIndicators items={homeSlides} activeIndex={activeIndex} onClickHandler={goToIndex} />
-        {slides}
-        <CarouselControl className={classes.controls} direction="prev" directionText="Previous" onClickHandler={previous} />
-        <CarouselControl className={classes.controls} direction="next" directionText="Next" onClickHandler={next} />
-      </Carousel>
+    <div>
+      <img src='\images\Home\ReffettAssociatesHome.jpg' alt='Reffett Associate Landing Page' className={classes.banner}/>
       <h4 className={classes.locations} onClick={handleClick}>Seattle | Washington D.C. | New York | Dallas | Greensboro</h4>
     </div>
   );
