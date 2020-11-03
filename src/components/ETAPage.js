a0bcb40e9163d2761a6fb7aaf00c9f3a8e373258
Charles Jarquio <cjarquio@gmail.com>
2020-11-03 15:01:01 -0800

    Removed redundant wording

--- src/components/ETAPage.js
+++ src/components/ETAPage.js
@@ -75,8 +75,6 @@ export default function ETAPage() {
       />
       <p>Are you ready for your next career move? In uncertain times or during a booming market you should always be ready for what the next opportunity or necessity brings. Take control of your career. </p>
       <p>Is your company preparing to downsize? </p>
-      <p>No opportunity for advancement?</p>
-      <p>Unhealthy work environment?</p>
       <p>Is it time to make the jump to the C-Suite?</p>
       <p>Are you ready to finally find that dream job?</p>
       <p>Reffett Associates’ team of experienced Managing Directors has been placing executives of all types into top jobs across a variety of private and public sector opportunities for years. We can help you take the steps to be ready for your next career transition.</p>
@@ -129,15 +127,6 @@ export default function ETAPage() {
           })
         }
       </List>
-      <ul><h5>Our Executive Transition Assistance program is discrete and high touch. Our senior staff will work directly with you to:</h5>
-        <li> Identify appropriate industry opportunities</li>
-        <li>Translate your unique skill set and experience into meaningful language</li>
-        <li>Create an update, modern, resume that connects with hiring authorities</li>
-        <li>Develop networking strategies</li>
-        <li>Refine your LinkedIn profile to attract attention and communicate your capabilities</li>
-        <li>Practice interview skills</li>
-        <li>And more.</li>
-      </ul>
       <p>On average it takes executives 9 months to make a career transition. Are you ready for your next step? Our team of experienced professionals is ready to help you find your next job faster.</p>
       <p><a href='mailto:tom.young@reffettassociates.com'>Contact Tom Young</a> for an introductory meeting to find out more.</p>
       <p>We are committed to serving Veterans and helping them achieve their career goals. <Link to='/veterans'>Learn More.</Link></p>
