e415b913c050d363ae1f1fa545f35cf917af0955
Charles Jarquio <cjarquio@gmail.com>
2022-08-17 10:01:32 -0700

    Add new office location

--- src/components/NewsPage.js
+++ src/components/NewsPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineOppositeContent, TimelineDot } from '@material-ui/lab';
 import { useRouteMatch } from 'react-router-dom';
@@ -8,74 +8,75 @@ import PageTitle from './PageTitle';
 import announcements from '../assets/data/announcements.json'
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: '6px 16px',
-  },
-  secondaryTail: {
-    backgroundColor: theme.palette.secondary.main,
-  },
+    paper: {
+        padding: '6px 16px',
+    },
+    secondaryTail: {
+        backgroundColor: theme.palette.secondary.main,
+    },
 }));
 
 
 export default function NewsPage() {
-  const classes = useStyles();
-  const [news , setNews] = useState([]);
-  let match = useRouteMatch();
+    const classes = useStyles();
+    const [news, setNews] = useState([]);
+    let match = useRouteMatch();
 
-  useEffect(()=>{
-     let mounted = true;
+    useEffect(() => {
+        let mounted = true;
 
-     if(mounted){
-          const sortedNews = announcements.sort(function (a, b) {
-               // Turn your strings into dates, and then subtract them
-               // to get a value that is either negative, positive, or zero.
-               return new Date(b.date) - new Date(a.date);
-          });
-          
-          
-          setNews(sortedNews)
-     }
+        if (mounted) {
+            const sortedNews = announcements.sort(function (a, b) {
+                // Turn your strings into dates, and then subtract them
+                // to get a value that is either negative, positive, or zero.
+                return new Date(b.date) - new Date(a.date);
+            });
 
-     return ()=>{
-          mounted = false;
-     }
-  },[])
 
-  return (
-    <div>
-      <PageTitle
-        title='News'
-        subtitle=' '
-      />
-      <Timeline align="alternate">
-        {
-          news.map((announcement) => {
-            return (
-              <TimelineItem>
-                <TimelineOppositeContent>
-                  <Typography variant="body">
-                    {announcement.date}
-                  </Typography>
-                </TimelineOppositeContent>
-                <TimelineSeparator>
-                  <TimelineDot color='primary'>
-                    <AnnouncementIcon />
-                  </TimelineDot>
-                  <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>
-                  <Paper elevation={3} className={classes.paper}>
-                    <Typography variant="h6" component="h1">
-                      {announcement.title}
-                    </Typography>
-                    <a href={announcement.external? announcement.link:`#${match.url}/${announcement.link}`}>{announcement.external? announcement.link:`http://reffettassociates.com/#${match.url}/${announcement.link}`}</a>
-                  </Paper>
-                </TimelineContent>
-              </TimelineItem>
-            )
-          })
+            setNews(sortedNews)
         }
-      </Timeline>
-    </div>
-  );
+
+        return () => {
+            mounted = false;
+        }
+    }, [])
+
+    return (
+        <div>
+            <PageTitle
+                title='News'
+                subtitle=' '
+            />
+            <p>Reffett Associates is pleased to be able to share the following stories and articles about our successful executive recruiting accomplishments. These articles showcase the wide range of clients that we work with and the wide variety of roles that we can fill.  The following links represent only a portion of the recruitments that our team has completed recently.</p>
+            <Timeline align="alternate">
+                {
+                    news.map((announcement) => {
+                        return (
+                            <TimelineItem>
+                                <TimelineOppositeContent>
+                                    <Typography variant="body">
+                                        {announcement.date}
+                                    </Typography>
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot color='primary'>
+                                        <AnnouncementIcon />
+                                    </TimelineDot>
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Paper elevation={3} className={classes.paper}>
+                                        <Typography variant="h6" component="h1">
+                                            {announcement.title}
+                                        </Typography>
+                                        <a href={announcement.external ? announcement.link : `#${match.url}/${announcement.link}`}>{announcement.external ? announcement.link : `http://reffettassociates.com/#${match.url}/${announcement.link}`}</a>
+                                    </Paper>
+                                </TimelineContent>
+                            </TimelineItem>
+                        )
+                    })
+                }
+            </Timeline>
+        </div>
+    );
 }
