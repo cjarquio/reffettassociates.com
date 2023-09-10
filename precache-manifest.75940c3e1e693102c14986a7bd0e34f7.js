4be73abf801dafa60f64c4630aabb538fee40b2e
Charles Jarquio <cjarquio@gmail.com>
2023-09-10 10:58:12 -0700

    Updates

---
+++ precache-manifest.75940c3e1e693102c14986a7bd0e34f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177b601529d802d6943fc9d2ab33ce70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "395c17fc3996dd20befb",
+    "url": "/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "a5448f2f6026fe6f1598",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "395c17fc3996dd20befb",
+    "url": "/static/js/2.a7447260.chunk.js"
+  },
+  {
+    "revision": "f52a96c81e538e6a05017d41c57f842a",
+    "url": "/static/js/2.a7447260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5448f2f6026fe6f1598",
+    "url": "/static/js/main.a6558894.chunk.js"
+  },
+  {
+    "revision": "395f6b0d7a41272ec210",
+    "url": "/static/js/runtime-main.5b4f8a4f.js"
+  }
+]);
