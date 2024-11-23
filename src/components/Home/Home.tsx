f5db85ab5df571e5e052c16a11b82a988fd76597
Charles Jarquio <cjarquio@gmail.com>
2024-11-22 17:27:21 -0800

    Added Reffett associates about to home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,60 @@
 import "@mantine/core/styles.css";
-import { Box } from "@mantine/core";
+import { Box, Container, Group, rem, Title } from "@mantine/core";
 import StockVideo from "../../../src/assets/videos/executiveStockVideo.mp4";
+import classes from "./Home.module.css";
+import { IconSearch } from "@tabler/icons-react";
 
 export const Home: React.FC = () => {
   return (
-    <Box style={{ display: "flex", width: "100%" }}>
+    <Box className={classes.root}>
       <video width={"100%"} autoPlay muted loop>
         <source src={StockVideo} type="video/mp4" />
       </video>
+      <Container fluid className={classes.executiveSearch}>
+        <Group justify={"flex-start"} gap={"sm"} style={{ flexWrap: "nowrap" }}>
+          <Box className={classes.executiveSearchTitle}>
+            <Title c={"#bb2a2c"} order={1}>
+              Executive Search
+            </Title>
+            <IconSearch
+              style={{ width: rem(80), height: rem(80) }}
+              stroke={3}
+              color={"#bb2a2c"}
+            />
+          </Box>
+          <Title
+            c={"white"}
+            order={4}
+            className={classes.executiveSearchDescription}
+          >
+            Reffett Associates is a nationally recognized, fully retained
+            executive search firm. As one of the premiere boutique firms in the
+            executive search industry, we work with you to build a stronger
+            organization by finding diverse candidates that fit your
+            organization's cultures and values.
+          </Title>
+        </Group>
+      </Container>
+      <Container fluid className={classes.veterans}>
+        <Group justify={"flex-end"} gap={"sm"} style={{ flexWrap: "nowrap" }}>
+          <Title c={"white"} order={4} className={classes.veteransDescription}>
+            Reffett Associates is proudly veteran-owned and a certified Veteran
+            Owned Small Business (VOSB). In addition, Reffett Associates is on
+            the GSA Schedule and maintains a GSA contract (GS-02F-0194W). We are
+            one of very few executive search firms to be awarded this
+            prestigious qualification after having completed the GSA's extensive
+            qualifications process.
+          </Title>
+          <Box className={classes.veteransTitle}>
+            <Title c={"#0f2f5b"} order={1}>
+              Committed to our Veterans
+            </Title>
+          </Box>
+        </Group>
+      </Container>
+      <Container size={"xl"} className={classes.clients}>
+        <Title order={3}>Featured Clients</Title>
+      </Container>
     </Box>
   );
 };
