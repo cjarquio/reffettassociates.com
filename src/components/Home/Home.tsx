6f4d31271024b4f7e02024e15558628e97fd47b1
Charles Jarquio <cjarquio@gmail.com>
2024-11-22 18:09:11 -0800

    Style ra about rows

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,7 +10,7 @@ export const Home: React.FC = () => {
       <video width={"100%"} autoPlay muted loop>
         <source src={StockVideo} type="video/mp4" />
       </video>
-      <Container fluid className={classes.executiveSearch}>
+      <Container fluid className={classes.raAboutContainer}>
         <Group justify={"flex-start"} gap={"sm"} style={{ flexWrap: "nowrap" }}>
           <Box className={classes.executiveSearchTitle}>
             <Title c={"#bb2a2c"} order={1}>
@@ -22,29 +22,37 @@ export const Home: React.FC = () => {
               color={"#bb2a2c"}
             />
           </Box>
-          <Title
-            c={"white"}
-            order={4}
-            className={classes.executiveSearchDescription}
-          >
-            Reffett Associates is a nationally recognized, fully retained
-            executive search firm. As one of the premiere boutique firms in the
-            executive search industry, we work with you to build a stronger
-            organization by finding diverse candidates that fit your
-            organization's cultures and values.
-          </Title>
+          <Box className={classes.executiveSearchDescriptionBox}>
+            <Title
+              c={"white"}
+              order={4}
+              className={classes.executiveSearchDescription}
+            >
+              Reffett Associates is a nationally recognized, fully retained
+              executive search firm. As one of the premiere boutique firms in
+              the executive search industry, we work with you to build a
+              stronger organization by finding diverse candidates that fit your
+              organization's cultures and values.
+            </Title>
+          </Box>
         </Group>
       </Container>
-      <Container fluid className={classes.veterans}>
+      <Container fluid className={classes.raAboutContainer}>
         <Group justify={"flex-end"} gap={"sm"} style={{ flexWrap: "nowrap" }}>
-          <Title c={"white"} order={4} className={classes.veteransDescription}>
-            Reffett Associates is proudly veteran-owned and a certified Veteran
-            Owned Small Business (VOSB). In addition, Reffett Associates is on
-            the GSA Schedule and maintains a GSA contract (GS-02F-0194W). We are
-            one of very few executive search firms to be awarded this
-            prestigious qualification after having completed the GSA's extensive
-            qualifications process.
-          </Title>
+          <Box className={classes.veteransDescriptionBox}>
+            <Title
+              c={"white"}
+              order={4}
+              className={classes.veteransDescription}
+            >
+              Reffett Associates is proudly veteran-owned and a certified
+              Veteran Owned Small Business (VOSB). In addition, Reffett
+              Associates is on the GSA Schedule and maintains a GSA contract
+              (GS-02F-0194W). We are one of very few executive search firms to
+              be awarded this prestigious qualification after having completed
+              the GSA's extensive qualifications process.
+            </Title>
+          </Box>
           <Box className={classes.veteransTitle}>
             <Title c={"#0f2f5b"} order={1}>
               Committed to our Veterans
