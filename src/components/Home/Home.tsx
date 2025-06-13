631866d338981c0c275bae8115cc3754cff828b1
Charles Jarquio <cjarquio@gmail.com>
2025-06-12 21:59:14 -0700

    Swap out Veteran Owned Icon

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,20 @@
-import "@mantine/core/styles.css";
-import { useRef } from "react";
-import Autoplay from "embla-carousel-autoplay";
-import { Box, Container, Group, rem, Title, Image } from "@mantine/core";
-import StockVideo from "../../../src/assets/videos/executiveStockVideo.mp4";
-import classes from "./Home.module.css";
-import { IconMilitaryRankFilled, IconSearch } from "@tabler/icons-react";
-import { Carousel } from "@mantine/carousel";
-import { ContactButtonSection } from "../Contact";
-import { ACE, NRECA } from "../../assets/images/companies/NonProfits";
-import { BECU } from "../../assets/images/companies/PrivateSector";
+import '@mantine/core/styles.css';
+import { useRef } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
+import { Box, Container, Group, rem, Title, Image } from '@mantine/core';
+import StockVideo from '../../../src/assets/videos/executiveStockVideo.mp4';
+import classes from './Home.module.css';
+import { IconSearch } from '@tabler/icons-react';
+import { Carousel } from '@mantine/carousel';
+import { ContactButtonSection } from '../Contact';
+import { ACE, NRECA } from '../../assets/images/companies/NonProfits';
+import { BECU } from '../../assets/images/companies/PrivateSector';
 import {
   BartellDrugs,
   SeattleMariners,
-} from "../../assets/images/companies/Retail";
-import { VA } from "../../assets/images/companies/PublicSector";
+} from '../../assets/images/companies/Retail';
+import { VA } from '../../assets/images/companies/PublicSector';
+import VOSB from '../../assets/images/veterans/VOSB.png';
 
 const images = [ACE, BartellDrugs, BECU, NRECA, SeattleMariners, VA];
 
@@ -21,31 +22,31 @@ export const Home: React.FC = () => {
   const autoplay = useRef(Autoplay({ delay: 4000 }));
 
   const slides = images.map((url) => (
-    <Carousel.Slide style={{ display: "flex", width: "100%" }} key={url}>
-      <Image style={{ objectFit: "contain" }} src={url} />
+    <Carousel.Slide style={{ display: 'flex', width: '100%' }} key={url}>
+      <Image style={{ objectFit: 'contain' }} src={url} />
     </Carousel.Slide>
   ));
 
   return (
     <Box className={classes.root}>
-      <video width={"100%"} autoPlay muted loop>
+      <video width={'100%'} autoPlay muted loop>
         <source src={StockVideo} type="video/mp4" />
       </video>
       <Container fluid className={classes.raAboutContainer}>
-        <Group justify={"flex-start"} gap={"sm"} style={{ flexWrap: "nowrap" }}>
+        <Group justify={'flex-start'} gap={'sm'} style={{ flexWrap: 'nowrap' }}>
           <Box className={classes.executiveSearchTitle}>
-            <Title c={"#bb2a2c"} order={1}>
+            <Title c={'#bb2a2c'} order={1}>
               Executive Search
             </Title>
             <IconSearch
               style={{ width: rem(80), height: rem(80) }}
               stroke={3}
-              color={"#bb2a2c"}
+              color={'#bb2a2c'}
             />
           </Box>
           <Box className={classes.executiveSearchDescriptionBox}>
             <Title
-              c={"white"}
+              c={'white'}
               order={4}
               className={classes.executiveSearchDescription}
             >
@@ -59,10 +60,10 @@ export const Home: React.FC = () => {
         </Group>
       </Container>
       <Container fluid className={classes.raAboutContainer}>
-        <Group justify={"flex-end"} gap={"sm"} style={{ flexWrap: "nowrap" }}>
+        <Group justify={'flex-end'} gap={'sm'} style={{ flexWrap: 'nowrap' }}>
           <Box className={classes.veteransDescriptionBox}>
             <Title
-              c={"white"}
+              c={'white'}
               order={4}
               className={classes.veteransDescription}
             >
@@ -75,19 +76,15 @@ export const Home: React.FC = () => {
             </Title>
           </Box>
           <Box className={classes.veteransTitle}>
-            <Title c={"black"} order={1}>
+            <Title c={'black'} order={1}>
               Veteran Owned
             </Title>
-            <IconMilitaryRankFilled
-              style={{ width: rem(132), height: rem(132) }}
-              stroke={3}
-              color={"black"}
-            />
+            <Image src={VOSB} p={'0.5rem'} w={rem(132)} h={rem(132)} />
           </Box>
         </Group>
       </Container>
-      <Container size={"md"} className={classes.clientsContainer}>
-        <Title order={2} style={{ textAlign: "center", padding: "2rem" }}>
+      <Container size={'md'} className={classes.clientsContainer}>
+        <Title order={2} style={{ textAlign: 'center', padding: '2rem' }}>
           Featured Clients
         </Title>
         <Carousel
