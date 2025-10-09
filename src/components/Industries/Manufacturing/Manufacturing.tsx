a183843a43dbd0a70f77ba3225e37fd05d1b2871
Charles Jarquio <cjarquio@gmail.com>
2025-10-09 16:29:49 -0700

    Use Template for manufacturing page

--- src/components/Industries/Manufacturing/Manufacturing.tsx
+++ src/components/Industries/Manufacturing/Manufacturing.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, Image, Text, Box, Card, Grid } from '@mantine/core';
+import { Text } from '@mantine/core';
 import { ManufacturingBanner } from '../../../assets/images';
 import {
   CastorAndPollux,
@@ -13,10 +13,10 @@ import {
   ItgBrands,
   CoreScientific,
 } from '../../../assets/images/companies/Manufacturing';
-import { Banner } from '../../Banner';
+import IndustryTemplate from '../IndustryTemplate/IndustryTemplate';
 
 export const Manufacturing: React.FC = () => {
-  const retailCompanies = [
+  const manufacturingCompanies = [
     {
       companyName: 'Castor & Pollux',
       image: CastorAndPollux,
@@ -63,53 +63,27 @@ export const Manufacturing: React.FC = () => {
     },
   ];
   return (
-    <>
-      <Banner bannerImagePath={ManufacturingBanner} page="Manufacturing" />
-      <Container fluid w="65%" display={'flex'}>
-        <Container w={'100%'}>
-          <Box id="retailDescription">
-            <Title order={3}>Manufacturing</Title>
-            <Text>
-              Success in today's market requires leaders and executives who
-              understand the nature of its rapidly changing landscape. Often
-              this means leveraging external partners such as Reffett Associates
-              to identify and attract exceptional talent that can make a
-              significant business impact.
-            </Text>
-            <Text>
-              The Reffett Associates team is uniquely positioned to understand
-              our clients' needs. Having served as senior executives in major
-              professional services and Fortune 500 companies, we possess deep
-              expertise in providing business solutions to commercial and
-              government clients. Our insight and consultative approach,
-              combined with access to the best talent, ensures that the results
-              we deliver to clients exceed their expectations.
-            </Text>
-          </Box>
-        </Container>
-        <Container w={'100%'}>
-          <Title order={3}>Companies Served</Title>
-          <Grid>
-            {retailCompanies.map((company) => (
-              <Grid.Col span={3} key={company.companyName}>
-                <Card padding="sm" style={{ alignItems: 'center' }}>
-                  <Card.Section>
-                    <Text fw={700}>{company.companyName}</Text>
-                  </Card.Section>
-                  <Card.Section>
-                    <Image
-                      src={company.image}
-                      fit="contain"
-                      alt={company.companyName}
-                    />
-                  </Card.Section>
-                </Card>
-              </Grid.Col>
-            ))}
-          </Grid>
-        </Container>
-      </Container>
-    </>
+    <IndustryTemplate
+      pageTitle="Manufacturing"
+      bannerPath={ManufacturingBanner}
+      industryCompanies={manufacturingCompanies}
+    >
+      <Text>
+        Success in today's market requires leaders and executives who understand
+        the nature of its rapidly changing landscape. Often this means
+        leveraging external partners such as Reffett Associates to identify and
+        attract exceptional talent that can make a significant business impact.
+      </Text>
+      <Text>
+        The Reffett Associates team is uniquely positioned to understand our
+        clients' needs. Having served as senior executives in major professional
+        services and Fortune 500 companies, we possess deep expertise in
+        providing business solutions to commercial and government clients. Our
+        insight and consultative approach, combined with access to the best
+        talent, ensures that the results we deliver to clients exceed their
+        expectations.
+      </Text>
+    </IndustryTemplate>
   );
 };
 
